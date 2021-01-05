814c6e04f4156e51bf1d539928a95a8d06406c91
Pierre Sid-idris <pierre.sididris@live.fr>
2021-01-05 23:29:13 +0100

    react & web3

---
+++ client/src/Main.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div id="content">
+        <h1>Ajouter un produit</h1>
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          const name = this.productName.value
+          const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
+          this.props.createProduct(name, price)
+        }}>
+          <div className="form-group mr-sm-2">
+            <input
+              id="productName"
+              type="text"
+              ref={(input) => { this.productName = input }}
+              className="form-control"
+              placeholder="Nom du produit"
+              required />
+          </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="productPrice"
+              type="text"
+              ref={(input) => { this.productPrice = input }}
+              className="form-control"
+              placeholder="Prix du produit"
+              required />
+          </div>
+          <button type="submit" className="btn btn-primary">Ajouter un produit</button>
+        </form>
+        <p> </p>
+        <h2>Acheter un produit</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nom</th>
+              <th scope="col">Prix</th>
+              <th scope="col">Vendeur</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody id="productList">
+            { this.props.products.map((product, key) => {
+              return(
+                <tr key={key}>
+                  <th scope="row">{product.id.toString()}</th>
+                  <td>{product.name}</td>
+                  <td>{window.web3.utils.fromWei(product.price.toString(), 'Ether')} Eth</td>
+                  <td>{product.owner}</td>
+                  <td>
+                    { !product.purchased
+                    ? <button class="btn btn-primary"
+                        name={product.id}
+                        value={product.price}
+                        onClick={(event) => {
+                          this.props.purchaseProduct(event.target.name, event.target.value)
+                        }}
+                      >
+                        Acheter
+                      </button>
+                    : <button class="btn btn-primary">remettre en vente</button>
+                    }
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Main;
