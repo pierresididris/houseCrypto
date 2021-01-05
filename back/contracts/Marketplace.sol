97d13ee31bfc7142b5d4ae430434281b04e15756
Pierre Sid-idris <pierre.sididris@live.fr>
2021-01-05 21:47:58 +0100

    smart contract

--- back/contracts/Marketplace.sol
+++ back/contracts/Marketplace.sol
@@ -1,48 +1,79 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.16;
 
-import "./RealEstateFactory.sol";
+contract Marketplace {
+    string public name;
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
 
-contract Marketplace is RealEstateFactory {
-    modifier onlyRealEstateForSale(uint _realEstateId) {
-        require(IndexOf(_realEstateId) >= 0);
-        require(msg.value == realEstates[_realEstateId].price);
-        _;
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address payable owner; //Parcqu'on veut payer le propriétaire, utiliser address payable
+        bool purchased;
     }
 
-    event newSale(uint realEstateId);
-    event deletedSale(uint realEstateId);
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
 
-    uint[] public marketPlace;
+    event ProductPurchased(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
 
-    function addRealEstate(uint realEstateId) public onlyOwnerOf(realEstateId) {
-        marketPlace.push(realEstateId);
-        emit newSale(realEstateId);
+    constructor() public {
+        name = "Marketplace";
     }
 
-    function removeRealEstate(uint realEstateId) public onlyOwnerOf(realEstateId) {
-        int index = IndexOf(realEstateId);
-        if (index >= 0) {
-            emit deletedSale(uint(realEstateId));
-            delete marketPlace[uint256(index)];
-        }
+    function createProduct(string memory _name, uint _price) public {
+        // Require a valid name
+        require(bytes(_name).length > 0);
+        // Require a valid price
+        require(_price > 0);
+        // Increment product count
+        productCount ++;
+        // Create the product
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        // Trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 
-    function transaction(uint256 _tokenId) public payable onlyRealEstateForSale(_tokenId) {
-        address _from = realEstateToOwner[_tokenId];
-        address(uint160(_from)).transfer(msg.value);
-        ownerRealEstateCount[_from] = ownerRealEstateCount[_from].sub(1);
-        ownerRealEstateCount[msg.sender] = ownerRealEstateCount[msg.sender].add(1);
-        realEstateToOwner[_tokenId] = msg.sender;
-        removeRealEstate(_tokenId);
+
+    //fonction payable => elle acceptera que la crypto-monnaie Etherum
+    function purchaseProduct(uint _id) public payable {
+        // Fetch the product
+        Product memory _product = products[_id];
+        // Fetch the owner
+        address payable _seller = _product.owner;
+        // Make sure the product has a valid id
+        require(_product.id > 0 && _product.id <= productCount);
+        // Require that there is enough Ether in the transaction
+        require(msg.value >= _product.price);
+        // Require that the product has not been purchased already
+        require(!_product.purchased);
+        // Require that the buyer is not the seller
+        require(_seller != msg.sender);
+        // Transfer ownership to the buyer
+        _product.owner = msg.sender;
+        // Mark as purchased
+        _product.purchased = true;
+        // Update the product
+        products[_id] = _product;
+        // Pay the seller by sending them Ether
+        address(_seller).transfer(msg.value);
+        // Trigger an event
+        emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
     }
 
-    function IndexOf(uint value) private view returns(int) {
-        for(uint index = 0; index < marketPlace.length; index++) {
-            if (marketPlace[index] == value) {
-                return int(index);
-            }
-        }
-        return -1;
-    }
+    //write resell function
+
 }
--- back/truffle-config.js
+++ back/truffle-config.js
@@ -1,12 +1,26 @@
 const path = require("path");
+require('babel-register');
+require('babel-polyfill');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  // contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  // contracts_build_directory: path.join(__dirname, "./src/abis"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+  },
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
 };
