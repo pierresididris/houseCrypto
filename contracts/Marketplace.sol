42ed374c495d57b23e963facf11170910e79696e
Pierre Sid-idris <pierre.sididris@live.fr>
2021-01-09 00:14:53 +0100

    marketplace smart contract done

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -4,28 +4,43 @@ pragma solidity >=0.5.16;
 contract Marketplace {
     string public name;
     uint public productCount = 0;
-    mapping(uint => Product) public products;
+    mapping(uint => RealEstate) public products;
 
-    struct Product {
+    struct RealEstate {
         uint id;
         string name;
         uint price;
+        string realEstateAddress;
+        uint area;
+        string description;
+        uint nbroom;
+        string sellingDate;
         address payable owner; //Parcqu'on veut payer le propriétaire, utiliser address payable
         bool purchased;
     }
 
-    event ProductCreated(
+    event RealEstateCreated(
         uint id,
         string name,
         uint price,
+        string realEstateAddress,
+        uint area,
+        string description,
+        uint nbroom,
+        string sellingDate,
         address payable owner,
         bool purchased
     );
 
-    event ProductPurchased(
+    event RealEstatePurchased(
         uint id,
         string name,
         uint price,
+        string realEstateAddress,
+        uint area,
+        string description,
+        uint nbroom,
+        string sellingDate,
         address payable owner,
         bool purchased
     );
@@ -34,24 +49,64 @@ contract Marketplace {
         name = "Marketplace";
     }
 
-    function createProduct(string memory _name, uint _price) public {
+    function createRealEstate(
+            string memory _name, 
+            uint _price,
+            string memory _realEstateAddress,
+            uint _area,
+            string memory _description, 
+            uint _nbroom,
+            string memory _sellingDate
+        ) public {
         // Require a valid name
         require(bytes(_name).length > 0);
         // Require a valid price
         require(_price > 0);
+        // Require a valid address of realestate
+        require(bytes(_realEstateAddress).length > 0);
+        // // Require a valid area
+        require(_area > 0);
+        // // Require a valid description of realestate
+        require(bytes(_description).length > 0);
+        // // Require a valid nbroom
+        require(_nbroom > 0);
+        // // Require a valid sellingDate of realestate
+        // require(bytes(_sellingDate).length > 0);
         // Increment product count
         productCount ++;
         // Create the product
-        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        products[productCount] = RealEstate(
+            productCount, 
+            _name, 
+            _price, 
+            _realEstateAddress, 
+            _area, 
+            _description, 
+            _nbroom, 
+            _sellingDate, 
+            msg.sender,
+            false
+        );
         // Trigger an event
-        emit ProductCreated(productCount, _name, _price, msg.sender, false);
+        emit RealEstateCreated(
+            productCount, 
+            _name, 
+            _price, 
+            _realEstateAddress, 
+            _area, 
+            _description, 
+            _nbroom, 
+            _sellingDate,  
+            msg.sender,
+            false
+        );
     }
 
 
-    //fonction payable => elle acceptera que la crypto-monnaie Etherum
-    function purchaseProduct(uint _id) public payable {
+    // fonction payable => elle acceptera que la crypto-monnaie Etherum
+    function purchaseRealEstate(uint _id) public payable {
         // Fetch the product
-        Product memory _product = products[_id];
+        RealEstate memory _product = products[_id];
         // Fetch the owner
         address payable _seller = _product.owner;
         // Make sure the product has a valid id
@@ -71,9 +126,19 @@ contract Marketplace {
         // Pay the seller by sending them Ether
         address(_seller).transfer(msg.value);
         // Trigger an event
-        emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
+        emit RealEstatePurchased(
+            productCount, 
+            _product.name, 
+            _product.price, 
+            _product.realEstateAddress, 
+            _product.area, 
+            _product.description, 
+            _product.nbroom, 
+            _product.sellingDate, 
+            msg.sender, 
+            true);
     }
 
-    //write resell function
+    // write resell function
 
 }
